41d08e665190abf1f1f6fb2baa1d1e9a49ba76b0
Sara <Sabera.daqiq@yahoo.com>
2016-11-23 14:10:02 -0800

    df

---
+++ cavestory1/grapgics.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <SDL.h>
+#include "graphics.h"
+
+//All info. dealing with graphics of the game
+
+Graphics::Graphics() {
+	SDL_CreateWindowAndRenderer(640, 480, 0, &this->_window, &this->_renderer); 
+	SDL_SetWindowTitle(this->_window, "Cavestory");
+}
+
+Graphics::~Graphics() {
+	SDL_DestroyWindow(this->_window);
+}
---
+++ cavestory1/main.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//#include <SDL.h>
+//using namespace std;
+#include "graphics.h"
+int main(int argc, char* args[]) {
+	Graphics graphics;
+	while (true) {
+		//sdfds
+
+	}
+	
+	return 0;
+}
